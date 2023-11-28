92114aaa0da7718030160bcee9827935887f1dec
sathish19961412 <sathishfreelanc5.com>
2023-11-28 12:56:38 -0500

    get route and display the view

---
+++ resources/views/contact.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Contact page</title>
+    </head>
+    <body>
+        <h2>Welcome to Contact page</h2>
+    </body>
+</html>
---
+++ resources/views/home.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Home page</title>
+    </head>
+    <body>
+        <h2>Welcome to Home page</h2>
+    </body>
+</html>
